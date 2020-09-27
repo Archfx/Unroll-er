de588999dc331213a80db3d0fab977de9085832a
Aruna Jayasena <aruna.15@cse.mrt.ac.lk>
2020-09-27 18:46:16 +0530

    flit buffer main loop unrolled

--- UnRoll-er.py
+++ UnRoll-er.py
@@ -30,27 +30,84 @@ for x in range (start_x,end_x):
 
         # print (unrolled)
 
-code ="""assign mask_0[(0+1)*OUT_WIDTH_0-1 : (0)*OUT_WIDTH_0]  =   {OUT_WIDTH_0{vc_num_wr[0]} };
-    assign mask_0[(1+1)*OUT_WIDTH_0-1 : (1)*OUT_WIDTH_0]  =   {OUT_WIDTH_0{vc_num_wr[1]} };
-    assign mask_0[(2+1)*OUT_WIDTH_0-1 : (2)*OUT_WIDTH_0]  =   {OUT_WIDTH_0{vc_num_wr[2]} };
-    assign mask_0[(3+1)*OUT_WIDTH_0-1 : (3)*OUT_WIDTH_0]  =   {OUT_WIDTH_0{vc_num_wr[3]} };
-
-    assign masked_mux_in_0    = wr_ptr_array & mask_0;
-
-    assign mux_out_gen_0 [0][0]   =   masked_mux_in_0[0+OUT_WIDTH_0*0];
-    assign mux_out_gen_0 [0][1]   =   masked_mux_in_0[0+OUT_WIDTH_0*1];
-    assign mux_out_gen_0 [0][2]   =   masked_mux_in_0[0+OUT_WIDTH_0*2];
-    assign mux_out_gen_0 [0][3]   =   masked_mux_in_0[0+OUT_WIDTH_0*3];
-    assign mux_out_gen_0 [1][0]   =   masked_mux_in_0[1+OUT_WIDTH_0*0];
-    assign mux_out_gen_0 [1][1]   =   masked_mux_in_0[1+OUT_WIDTH_0*1];
-    assign mux_out_gen_0 [1][2]   =   masked_mux_in_0[1+OUT_WIDTH_0*2];
-    assign mux_out_gen_0 [1][3]   =   masked_mux_in_0[1+OUT_WIDTH_0*3];
+code ="""assign  wr_ptr_array[(i+1)*Bw- 1        :   i*Bw]   =       wr_ptr[i];
+        assign  rd_ptr_array[(i+1)*Bw- 1        :   i*Bw]   =       rd_ptr[i];
+        //assign    vc_nearly_full[i] = (depth[i] >= B-1);
+        assign  vc_not_empty    [i] =   (depth[i] > 0);
     
-    assign vc_wr_addr[0] = | mux_out_gen_0 [0];
-    assign vc_wr_addr[1] = | mux_out_gen_0 [1];"""
+    
+        always @(posedge clk or posedge reset) begin
+            if (reset) begin
+                rd_ptr  [i] <= {Bw{1'b0}};
+                wr_ptr  [i] <= {Bw{1'b0}};
+                depth   [i] <= {DEPTHw{1'b0}};
+            end
+            else begin
+                if (wr[i] && depth[i] != B) wr_ptr[i] <= wr_ptr [i]+ 1'h1;
+                if (rd[i] && (depth[i] != {DEPTHw{1'b0}})) rd_ptr [i]<= rd_ptr [i]+ 1'h1;
+                if (wr[i] & ~rd[i]) depth [i]<=
+                //synthesis translate_off
+                //synopsys  translate_off
+                   #1
+                //synopsys  translate_on
+                //synthesis translate_on
+                   depth[i] + 1'h1;
+                else if (~wr[i] & rd[i]) depth [i]<=
+                //synthesis translate_off
+                //synopsys  translate_off
+                   #1
+                //synopsys  translate_on
+                //synthesis translate_on
+                   depth[i] - 1'h1;
+            end//else
+        end//always
 
-find_this ="_0"
-replace_this = "_1"
+        //synthesis translate_off
+        //synopsys  translate_off
+    
+        always @(posedge clk) begin
+            if(~reset)begin
+                if (wr[i] && (depth[i] == B) && !rd[i])
+                    $display("%t: ERROR: Attempt to write to full FIFO:FIFO size is %d. %m",$time,B);
+                /* verilator lint_off WIDTH */
+                if (rd[i] && (depth[i] == {DEPTHw{1'b0}} &&  SSA_EN !="YES"  ))
+                    $display("%t: ERROR: Attempt to read an empty FIFO: %m",$time);
+                if (rd[i] && !wr[i] && (depth[i] == {DEPTHw{1'b0}} &&  SSA_EN =="YES" ))
+                    $display("%t: ERROR: Attempt to read an empty FIFO: %m",$time);
+                /* verilator lint_on WIDTH */
+          
+            end//~reset      
+        end//always
+
+     
+            // Asserting the Property b1 : Read and write pointers are incremented when r_en/w_en are set
+            // Asserting the property b3 : Read and Write pointers are not incremented when the buffer is empty and full
+            // Asserting the property b4 : Buffer can not be both full and empty at the same time
+                            
+        // Branch statements
+        always@(posedge clk) begin
+            //b1.1
+            if (wr[i] && depth[i] != B && !reset) begin
+                wr_ptr_check[i] <= wr_ptr[i];
+            end  
+            //b1.2
+            if (rd[i] && (depth[i] != {DEPTHw{1'b0}}) && !reset) begin
+                rd_ptr_check[i] <= rd_ptr[i];
+            end
+            //b3.1 trying to write to full buffer
+            if (wr[i] & ~rd[i] && (depth[i] == B) && !reset) begin
+                wr_ptr_check[i] <= wr_ptr[i];
+            end
+            //b3.2 trying to read from empty buffer
+            if (rd[i] && !wr[i] && (depth[i] == {DEPTHw{1'b0}}) && !reset) begin
+                rd_ptr_check[i] <= rd_ptr[i];
+            end
+        end            
+            
+    end//for"""
+
+find_this ="[i]"
+replace_this = ""
 
 replaced=code.replace(find_this, replace_this)
 print (replaced)
